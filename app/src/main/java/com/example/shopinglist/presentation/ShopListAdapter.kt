42a566b208619e8489be42c47465d404d09f5190
per-sever <a.petrin107@gmail.com>
2022-10-23 20:38:28 +0300

    adapter was fixed

--- app/src/main/java/com/example/shopinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shopinglist/presentation/ShopListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.example.shopinglist.R
@@ -11,7 +12,11 @@ import com.example.shopinglist.domain.ShopItem
 
 class ShopListAdapter : Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopItem>()
+    var list = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
@@ -23,11 +28,30 @@ class ShopListAdapter : Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
         val shopItem = list[position]
-        holder.tvName.text = shopItem.name
-        holder.tvCount.text = shopItem.count.toString()
+        val status = if (shopItem.enabled) {
+            "Active"
+        } else {
+            "Not Active"
+        }
+        if (shopItem.enabled) {
+            holder.tvName.text = "${shopItem.name} $status"
+            holder.tvCount.text = shopItem.count.toString()
+            holder.tvName.setTextColor(
+                ContextCompat.getColor(
+                    holder.view.context, android.R
+                        .color.holo_red_dark
+                )
+            )
+        }
         holder.view.setOnLongClickListener {
             true
         }
+    }
+
+    override fun onViewRecycled(holder: ShopItemViewHolder) {
+        super.onViewRecycled(holder)
+        holder.tvName.text = ""
+        holder.tvCount.text = ""
     }
 
     override fun getItemCount(): Int {
